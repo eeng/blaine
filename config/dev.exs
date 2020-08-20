f95406a5ecbeebb7064c57fbe7b458e1f25cda13
Emmanuel Nicolau <emmanicolau@gmail.com>
2020-08-20 10:05:46 -0300

    Implement the renew_token function

--- config/dev.exs
+++ config/dev.exs
@@ -1,5 +1,3 @@
 import Config
 
-config :tesla, Tesla.Middleware.Logger, debug: false
-
 import_config "#{Mix.env()}.secrets.exs"
--- lib/watch_later/google_auth.ex
+++ lib/watch_later/google_auth.ex
@@ -1,17 +1,15 @@
 defmodule WatchLater.GoogleAuth do
   @config Application.get_env(:watch_later, __MODULE__)
 
+  use Tesla
+
+  plug Tesla.Middleware.BaseUrl, "https://oauth2.googleapis.com"
+  plug Tesla.Middleware.FormUrlencoded
+  plug Tesla.Middleware.DecodeJson
+  plug Tesla.Middleware.Logger
+
   defmodule Token do
     defstruct [:access_token, :expires_in, :refresh_token, :token_type]
-  end
-
-  def client do
-    Tesla.client([
-      {Tesla.Middleware.BaseUrl, "https://oauth2.googleapis.com"},
-      Tesla.Middleware.FormUrlencoded,
-      Tesla.Middleware.DecodeJson,
-      Tesla.Middleware.Logger
-    ])
   end
 
   def authorize_url(params \\ []) do
@@ -25,16 +23,33 @@ defmodule WatchLater.GoogleAuth do
     "https://accounts.google.com/o/oauth2/v2/auth?" <> query
   end
 
-  def get_token(client, code) do
-    body = %{
+  def get_token(code) do
+    post_request(%{
       code: code,
       client_id: @config[:client_id],
       client_secret: @config[:client_secret],
       redirect_uri: "urn:ietf:wg:oauth:2.0:oob",
       grant_type: "authorization_code"
-    }
+    })
+  end
 
-    case Tesla.post(client, "/token", body) do
+  def renew_token(%Token{refresh_token: refresh_token}) do
+    token =
+      post_request(%{
+        client_id: @config[:client_id],
+        client_secret: @config[:client_secret],
+        refresh_token: refresh_token,
+        grant_type: "refresh_token"
+      })
+
+    # Re-insert the refresh_token as it doesn't comes back in the response
+    with {:ok, token} <- token do
+      {:ok, %{token | refresh_token: refresh_token}}
+    end
+  end
+
+  defp post_request(body) do
+    case post("/token", body) do
       {:ok, %{status: 200, body: body}} -> {:ok, to_struct(Token, body)}
       {:ok, %{body: body}} -> {:error, body}
       error -> error

73a5a4e90781cfe5ecd14050dc482db24abc048c
Emmanuel Nicolau <emmanicolau@gmail.com>
2020-08-20 13:24:38 -0300

    Create a YouTube module

--- lib/watch_later/google_auth.ex
+++ lib/watch_later/google_auth.ex
@@ -1,17 +1,17 @@
 defmodule WatchLater.GoogleAuth do
   @config Application.get_env(:watch_later, __MODULE__)
 
+  defmodule Token do
+    defstruct [:access_token, :expires_in, :refresh_token, :token_type]
+    use ExConstructor
+  end
+
   use Tesla
 
   plug Tesla.Middleware.BaseUrl, "https://oauth2.googleapis.com"
   plug Tesla.Middleware.FormUrlencoded
   plug Tesla.Middleware.DecodeJson
   plug Tesla.Middleware.Logger
-
-  defmodule Token do
-    defstruct [:access_token, :expires_in, :refresh_token, :token_type]
-    use ExConstructor
-  end
 
   def authorize_url(params \\ []) do
     defaults = %{
@@ -25,23 +25,25 @@ defmodule WatchLater.GoogleAuth do
   end
 
   def get_token(code) do
-    post_request(%{
+    post("/token", %{
       code: code,
       client_id: @config[:client_id],
       client_secret: @config[:client_secret],
       redirect_uri: "urn:ietf:wg:oauth:2.0:oob",
       grant_type: "authorization_code"
     })
+    |> handle_response()
   end
 
   def renew_token(%Token{refresh_token: refresh_token}) do
     token =
-      post_request(%{
+      post("/token", %{
         client_id: @config[:client_id],
         client_secret: @config[:client_secret],
         refresh_token: refresh_token,
         grant_type: "refresh_token"
       })
+      |> handle_response()
 
     # Re-insert the refresh_token as it doesn't comes back in the response
     with {:ok, token} <- token do
@@ -49,8 +51,8 @@ defmodule WatchLater.GoogleAuth do
     end
   end
 
-  defp post_request(body) do
-    case post("/token", body) do
+  defp handle_response(response) do
+    case response do
       {:ok, %{status: 200, body: body}} -> {:ok, Token.new(body)}
       {:ok, %{body: body}} -> {:error, body}
       error -> error
---
+++ lib/watch_later/youtube.ex
@@ -0,0 +1,24 @@
+defmodule WatchLater.YouTube do
+  alias WatchLater.GoogleAuth.Token
+
+  def client(%Token{access_token: access_token, token_type: token_type}) do
+    Tesla.client([
+      {Tesla.Middleware.BaseUrl, "https://www.googleapis.com/youtube/v3"},
+      {Tesla.Middleware.Headers, [{"Authorization", "#{token_type} #{access_token}"}]},
+      Tesla.Middleware.JSON,
+      Tesla.Middleware.Logger
+    ])
+  end
+
+  def list_subscriptions(client, params \\ []) do
+    client |> Tesla.get("/subscriptions", query: params) |> handle_response()
+  end
+
+  defp handle_response(response) do
+    case response do
+      {:ok, %{status: 200, body: body}} -> {:ok, body}
+      {:ok, %{body: %{"error" => error}}} -> {:error, error}
+      error -> error
+    end
+  end
+end

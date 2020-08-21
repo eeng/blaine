b2cc800e46c4aef31ef47b697ab5fd1abe25d3c1
Emmanuel Nicolau <emmanicolau@gmail.com>
2020-08-21 17:07:02 -0300

    Move the @config to a function

--- lib/watch_later/google/auth_api.ex
+++ lib/watch_later/google/auth_api.ex
@@ -1,6 +1,4 @@
 defmodule WatchLater.Google.AuthAPI do
-  @config Application.get_env(:watch_later, __MODULE__)
-
   alias WatchLater.Google.AuthToken
   use Tesla
 
@@ -9,9 +7,11 @@ defmodule WatchLater.Google.AuthAPI do
   plug Tesla.Middleware.DecodeJson
   plug Tesla.Middleware.Logger
 
+  defp config(key), do: Application.get_env(:watch_later, __MODULE__)[key]
+
   def authorize_url(params \\ []) do
     defaults = %{
-      client_id: @config[:client_id],
+      client_id: config(:client_id),
       response_type: "code",
       redirect_uri: "urn:ietf:wg:oauth:2.0:oob"
     }
@@ -23,8 +23,8 @@ defmodule WatchLater.Google.AuthAPI do
   def get_token(code) do
     body = %{
       code: code,
-      client_id: @config[:client_id],
-      client_secret: @config[:client_secret],
+      client_id: config(:client_id),
+      client_secret: config(:client_secret),
       redirect_uri: "urn:ietf:wg:oauth:2.0:oob",
       grant_type: "authorization_code"
     }
@@ -36,8 +36,8 @@ defmodule WatchLater.Google.AuthAPI do
 
   def renew_token(%AuthToken{refresh_token: refresh_token}) do
     body = %{
-      client_id: @config[:client_id],
-      client_secret: @config[:client_secret],
+      client_id: config(:client_id),
+      client_secret: config(:client_secret),
       refresh_token: refresh_token,
       grant_type: "refresh_token"
     }

a961382d73c658b15e22f61191b31673dd3c2f95
Emmanuel Nicolau <emmanicolau@gmail.com>
2020-08-23 18:48:26 -0300

    Remove some unused modules

--- lib/watch_later/persistance/accounts_repository.ex
+++
@@ -1,9 +0,0 @@
-defmodule WatchLater.Persistance.AccountsRepository do
-  alias WatchLater.Entities.Account
-
-  @type state :: [Account.t()]
-
-  @callback store_accounts(state) :: any
-  @callback fetch_accounts() :: state
-  @callback close() :: any
-end

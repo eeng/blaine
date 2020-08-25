12e8b2e5546ca1c9881bcbae8c1dd9470e1595bc
Emmanuel Nicolau <emmanicolau@gmail.com>
2020-08-25 12:40:30 -0300

    Make the UploadScanner test more robust

--- test/watch_later/jobs/uploads_scanner_test.exs
+++ test/watch_later/jobs/uploads_scanner_test.exs
@@ -7,33 +7,31 @@ defmodule WatchLater.Jobs.UploadsScannerTest do
   import Mox
   setup :verify_on_exit!
 
-  describe "periodic execution" do
+  describe "checkpoint execution" do
     test "calls the service with the last_run_at and then updates it", context do
       t1 = DateTime.utc_now()
-      process_opts = [name: context.test, interval: 50, last_run_at: t1]
+      process_opts = [name: context.test, last_run_at: t1]
       scanner = start_supervised!({UploadsScanner, process_opts})
 
-      wait_until_service_is_called(scanner, t1)
+      expect_service_called(scanner, t1)
+      send(scanner, :work)
       %{last_run_at: t2} = :sys.get_state(scanner)
       assert :gt = DateTime.compare(t2, t1)
 
-      wait_until_service_is_called(scanner, t2)
+      expect_service_called(scanner, t2)
+      send(scanner, :work)
       %{last_run_at: t3} = :sys.get_state(scanner)
       assert :gt = DateTime.compare(t3, t2)
     end
 
-    defp wait_until_service_is_called(scanner, published_after) do
+    defp expect_service_called(scanner, published_after) do
       parent = self()
-      ref = make_ref()
 
       MockUploadsService
       |> allow(parent, scanner)
       |> expect(:find_uploads_and_add_to_watch_later, fn published_after: ^published_after ->
-        send(parent, ref)
         {:ok, 0}
       end)
-
-      assert_receive ^ref
     end
   end
 end

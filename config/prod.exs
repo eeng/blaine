d7ec81908077a67d40751320d48cbaed3b3a493a
Emmanuel Nicolau <emmanicolau@gmail.com>
2020-08-25 13:38:23 -0300

    Prepare for deploy

--- config/prod.exs
+++ config/prod.exs
@@ -1,5 +1,9 @@
 import Config
 
+config :watch_later, WatchLater.Google.AuthAPI,
+  client_id: System.fetch_env!("GOOGLE_CLIENT_ID"),
+  client_secret: System.fetch_env!("GOOGLE_CLIENT_SECRET")
+
 config :watch_later, WatchLater.Jobs.UploadsScanner,
   interval: System.get_env("SCANNER_INTERVAL", 60 * 60)
 
